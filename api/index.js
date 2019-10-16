9d81ae3eb4edf9ff35e2bc06c5f34ede474f704e
Kjell Erik Reed Anda <keaaa@statoil.com>
2019-10-16 09:21:17 +0200

    api authorize requests

--- api/index.js
+++ api/index.js
@@ -5,29 +5,13 @@ const PORT = process.env.PORT || 8002;
 const HOST = process.env.HOST || '0.0.0.0';
 const app = express();
 const jwt = require('jsonwebtoken');
-
-const azureAdPublicKey = `
------BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAp2DzxOZiWEHhtVavuwIm
-ryTRxW4kJ0mbA1lbXon550DUnKDZCNZaztno8HpOl6NSbVbW+QLDz5VOqCn+PDvS
-IRcw+2hrJPRnCNob4yGEuC7v9dPVpPDFRiUrOcwCbJak6xsK9PEsX8FQ/onFHO6Y
-JkjsFG8S2nMhgRK+JdURUcuj9paywSBtW9ddeqjQPgCPbZJtk39ReouoBYNm9xiw
-hTN0InY9Rt9PKUh4cRetg3OeKQ2E8TOVh1nHeTT2HIIYnAgB7ESUA07wYBuvet4U
-GemC2SdfpTSWk2YqzjZONW8p01hJg9x8lcSeyaQVOxTP/SjQoP99la1V8lArF35q
-xQIDAQAB
------END PUBLIC KEY-----
-`
+const azureADPublicKey = process.env.AZURE_AD_PUBLIC_KEY.replace(/\\n/g, "\n");
+const resourceID = process.env.API_RESOURCE_ID;
 
 // Generic request handler
 app.get('*', (req, res) => {
   console.log(`Request received by the API: ${req.method} ${req.originalUrl}`);
-  const token = req.header('authorization').replace("Bearer ", "");
-  console.log(`token: ${token}`);
-  try {
-    const decoded = jwt.verify(token, azureAdPublicKey);
-    console.log(`decoded token: ${decoded}`);
-  } catch(err){
-    console.log(err);
+  if (!isAuthorized(req, ["Radix"])){
     res.sendStatus(403);
     return;
   }
@@ -38,9 +22,31 @@ app.get('*', (req, res) => {
   `;
 
   res.send(output);
-  console.log(req.method, req.originalUrl);
 });
 
 // Start server
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+console.log(azureADPublicKey);
+
+/**
+ * authorize request
+ * req - request Request<Dictionary<string>>
+ * [roles] - array of roles. If empty skip check. The token is authorized if it has any of the roles
+ * returns - isAuthorized = true/false.
+ */
+const isAuthorized = (req, roles) => {
+  const token = req.header('authorization').replace("Bearer ", "");
+  let isAuthorized = false;
+  try {
+    const decodedToken = jwt.verify(token, azureADPublicKey, { audience: resourceID } );
+    if (roles && roles.length > 0) {
+      isAuthorized = decodedToken.roles && roles.some(role => decodedToken.roles.some(userRole => userRole === role));
+    } else {
+      isAuthorized = true;
+    }
+  } catch(err){
+    console.log(err);
+  }
+  return isAuthorized;
+}
