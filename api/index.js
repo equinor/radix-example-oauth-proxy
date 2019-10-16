7a2fe24467d6c49ae5e4172fc762f4f2ff714fa9
Kjell Erik Reed Anda <kjell.erik.anda@gmail.com>
2019-10-16 12:30:59 +0200

    Expand example with protected api
    Expand example with:
    - during authentication also creates an access_token for api (not just ID token)
    - access_token is forwarded to api
    - verified that access_token is made with correct scope, audience and that it contains roles
    - api now authorize request
    - api is now open for direct call (since it protect itself)

--- api/index.js
+++ api/index.js
@@ -4,18 +4,49 @@ const express = require('express');
 const PORT = process.env.PORT || 8002;
 const HOST = process.env.HOST || '0.0.0.0';
 const app = express();
+const jwt = require('jsonwebtoken');
+const azureADPublicKey = process.env.AZURE_AD_PUBLIC_KEY.replace(/\\n/g, "\n");
+const resourceID = process.env.API_RESOURCE_ID;
 
 // Generic request handler
 app.get('*', (req, res) => {
+  console.log(`Request received by the API: ${req.method} ${req.originalUrl}`);
+  if (!isAuthorized(req, ["Radix"])){
+    res.sendStatus(403);
+    return;
+  }
+
   let output = `
     Request received by the API: ${req.method} ${req.originalUrl}
     Headers: ${JSON.stringify(req.headers, null, 2)}
   `;
 
   res.send(output);
-  console.log(req.method, req.originalUrl);
 });
 
 // Start server
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+console.log(azureADPublicKey);
+
+/**
+ * authorize request
+ * req - request Request<Dictionary<string>>
+ * [roles] - array of roles. If empty skip check. The token is authorized if it has any of the roles
+ * returns - isAuthorized = true/false.
+ */
+const isAuthorized = (req, roles) => {
+  const token = req.header('authorization').replace("Bearer ", "");
+  let isAuthorized = false;
+  try {
+    const decodedToken = jwt.verify(token, azureADPublicKey, { audience: resourceID } );
+    if (roles && roles.length > 0) {
+      isAuthorized = decodedToken.roles && roles.some(role => decodedToken.roles.some(userRole => userRole === role));
+    } else {
+      isAuthorized = true;
+    }
+  } catch(err){
+    console.log(err);
+  }
+  return isAuthorized;
+}
